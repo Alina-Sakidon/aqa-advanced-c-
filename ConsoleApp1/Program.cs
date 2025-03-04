f51df1f01f3e86ec95214502466560c9693af373
asakidon <alinasakidon@gmail.com>
2025-03-04 13:55:37 +0200

    task1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//task1
+string str = "Привіт, світ!";  
+int num = 42;                  
+bool boolVar = true;           
+object nullVar = null;         
+ 
+// Виведення значень змінних на консоль
+Console.WriteLine("Рядок: " + str);
+Console.WriteLine("Число: " + num);
+Console.WriteLine("Буль: " + boolVar);
+Console.WriteLine("Null: " + (nullVar ?? "null"));
+
